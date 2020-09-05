a8ebac559c285c965dd30f7b6c926a8bfe7dd440
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2020-09-05 10:50:34 -0700

    yt player being called in

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ const youTubeUrl = {
     vidUrls: '',
     URL: 'https://www.googleapis.com/youtube/v3/playlistItems?playlistId='+youTubeCreds.playListId[0]+'&key='+youTubeCreds.key
 };
+
+const retrievePlayer = {
+    retrieveVideos: function(){
+        $.getJSON(youTubeUrl.URL, options, function(data){
+            youTubeCreds.firstVid = data.items[0].snippet.resourceId.videoId;
+            console.log(data.items);
+            playMainVideo.mainVid(youTubeCreds.firstVid);
+        });
+    },
+};
+
+const playMainVideo = {
+    mainVid: function(firstVid){
+        $('#main-video-player-0').html(
+            `<div class="aspect-ratio">
+                <iframe class="" width="auto" height="600px" src="https://www.youtube-nocookie.com/embed/'${firstVid}?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>`
+        );
+    },
+}
+
+retrievePlayer.retrieveVideos();
