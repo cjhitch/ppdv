712dbd4b2dfa6b81b8033b390781612bf58ef21f
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2020-09-05 11:08:23 -0700

    fixed class, added back missing quote

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const retrievePlayer = {
     retrieveVideos: function(){
         $.getJSON(youTubeUrl.URL, options, function(data){
             youTubeCreds.firstVid = data.items[0].snippet.resourceId.videoId;
+            console.log(youTubeCreds.firstVid)
             console.log(data.items);
             playMainVideo.mainVid(youTubeCreds.firstVid);
         });
@@ -29,11 +30,9 @@ const retrievePlayer = {
 
 const playMainVideo = {
     mainVid: function(firstVid){
-        $('#main-video-player-0').html(
-            `<div class="aspect-ratio">
-                <iframe class="" width="auto" height="600px" src="https://www.youtube-nocookie.com/embed/'${firstVid}?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>`
-        );
+        document.querySelector('#main-video-player-0').innerHTML = `<div class="">
+            <iframe class="" width="1050px" height="600px" src="https://www.youtube-nocookie.com/embed/${firstVid}?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe>
+        </div>`
     },
 }
 
