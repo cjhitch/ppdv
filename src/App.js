15236b0a354534e74c781eb95f0f77d7f733c031
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2020-09-07 18:28:37 -0700

    added in stuff from script

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const key = 'AIzaSyA54SI8UXryZuA4xbFhu6YzCXYnSGCPFuU';
+	const plId = ['PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU'];
+
+	const [yTUrl, setYTUrl] = useState()
+
+	// Credentials for youtube - added in 
+const youTubeCreds = {
+    key: 'AIzaSyA54SI8UXryZuA4xbFhu6YzCXYnSGCPFuU',
+    playListId: ['PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU'],
+}
+
+const youTubeUrl = {
+    URL: 'https://www.googleapis.com/youtube/v3/playlistItems?playlistId='+youTubeCreds.playListId[0]+'&key='+youTubeCreds.key
+};
+
+const options = {
+    part: 'snippet',
+    key: youTubeCreds.key,
+    maxResults: 20,
+    playlistId: youTubeCreds.playListId
+};
+
+const tubeItems = {
+    videoIds: [],
+    titles: [],
+}
+
+	return <div className="App">
+		<h1>
+			Select a Video:
+		</h1>
+		<select name="vid_id" id="vid_id">
+
+		</select>
+	</div>;
 }
 
 export default App;
