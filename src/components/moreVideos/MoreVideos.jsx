c84cff48033ac3e97a5aa7d98f4735f1589f3e03
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2020-09-12 21:18:04 -0700

    initial set up for more videos

---
+++ src/components/moreVideos/MoreVideos.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MoreVideos.scss';
+
+const MoreVideos = () => {
+    return (
+        <section>
+            <h3>
+                More Videos
+            </h3>
+            <p>
+                Select a new video from the list
+            </p>
+        </section>
+    )
+}
+
+export default MoreVideos
