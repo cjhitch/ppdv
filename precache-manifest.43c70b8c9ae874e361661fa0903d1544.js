67a315b863216c2a69d0fffe7580749b574e2029
Christopher Hitchcock <chitchcock@student.fullsail.edu>
2020-09-23 10:41:17 -0700

    Updates

---
+++ precache-manifest.43c70b8c9ae874e361661fa0903d1544.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67e211c5ed668165ee976e34437cfad",
+    "url": "/ppdv/index.html"
+  },
+  {
+    "revision": "d21be1091242c9083678",
+    "url": "/ppdv/static/css/main.f5a4e216.chunk.css"
+  },
+  {
+    "revision": "b8d9a6b31e8b3362a733",
+    "url": "/ppdv/static/js/2.40f1eff8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ppdv/static/js/2.40f1eff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21be1091242c9083678",
+    "url": "/ppdv/static/js/main.3ec74ff5.chunk.js"
+  },
+  {
+    "revision": "f52f04216dd615230eef",
+    "url": "/ppdv/static/js/runtime-main.2c05e293.js"
+  },
+  {
+    "revision": "474e8a4065b5a288e16b90f60685409d",
+    "url": "/ppdv/static/media/bulb.474e8a40.svg"
+  },
+  {
+    "revision": "fd3ce20b1d077723348d5e7e5c9b4e5e",
+    "url": "/ppdv/static/media/hero.fd3ce20b.jpeg"
+  },
+  {
+    "revision": "f5d5f4d4fd19765da0b32739970e5425",
+    "url": "/ppdv/static/media/logo-dark.f5d5f4d4.svg"
+  },
+  {
+    "revision": "e23dd7eb5e75a40fa7585f19fda2d8e5",
+    "url": "/ppdv/static/media/logo.e23dd7eb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e897ea7c794d80984cae23782c953b0.js"
+  "/ppdv/precache-manifest.43c70b8c9ae874e361661fa0903d1544.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ppdv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
